1158daec634e6d22ea06adf321d62bbca8f5c959
gpsmatty <gpsmatty@gmail.com>
2016-10-25 13:43:37 -0700

    adding requirements to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,16 @@ class PostsController < ApplicationController
   end
   def new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to @post
+  end
+
+  private
+      def post_params
+        params.require(:post).permit(tittle, :body)
+      end
 end
